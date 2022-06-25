81a725a5bb8c9234f80c6e3666bfc2ad49110c44
Jerry Lott <jerry.l.lott@gmail.com>
2022-06-25 00:52:01 -0500

    Added nav bar with a few styles

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+export default function Header() {
+  return ( 
+    <header className="header" id="header">
+      <nav className="nav" id="nav">
+        <a href="#" className="nav__title">The Laundry Station</a>
+        <ul className="nav__list">
+          <li>
+            <a href="#home" className="nav__item">Home</a>
+          </li>
+          <li>
+            <a href="#about" className="nav__item">About</a>
+          </li>
+          <li>
+            <a href="#services" className="nav__item">Services</a>
+          </li>
+          <li>
+            <a href="#contact" className="nav__item">Contact Us</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
