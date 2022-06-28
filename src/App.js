a9e327fb38733adaeef68b7f50693496976d86dc
Jerry Lott <jerry.l.lott@gmail.com>
2022-06-27 22:25:05 -0500

    Added services.js and headings for services

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/Header"
 import Home from "./pages/Home"
 import About from "./pages/About"
+import Services from "./pages/Services"
 import Footer from "./components/Footer"
 
 export default function App() {
@@ -9,6 +10,7 @@ export default function App() {
       <Header />
       <Home />
       <About />
+      <Services />
       <Footer />
     </div>
   )
---
+++ src/pages/Services.js
@@ -0,0 +1,9 @@
+import Heading from "../components/Heading";
+
+export default function Services() {
+  return (
+    <div className="services" id="services">
+      <Heading title="Services" subtitle="Professional services for all your laundry needs."/>
+    </div>
+  )
+}
