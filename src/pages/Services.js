abd645b7537a2dc40a4c5999e0150eaaa6871511
Jerry Lott <jerry.l.lott@gmail.com>
2022-06-30 11:11:01 -0500

    Added folded clothes img and title to services page

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,9 +1,24 @@
 import Heading from "../components/Heading";
+import FoldedClothes from '../assets/img/folded-clothes.jpg'
 
 export default function Services() {
   return (
-    <div className="services" id="services">
-      <Heading title="Services" subtitle="Professional services for all your laundry needs."/>
-    </div>
+    <section className="services" id="services">
+      <Heading title="Services" subtitle="Professional service for all your laundry needs."/>
+      <div className="services__content">
+        <div className="services-content__header">
+          <img 
+            src={FoldedClothes} 
+            alt="Folded Clothes" 
+            className="services-header__img"
+          />
+          <div className="services-header__title">Drop-off</div>
+        </div>
+        <div className="services-content__body">
+          
+        </div>
+      </div>
+    </section>
+    
   )
 }
