844ffd802568c821ce8d26fb01141718ae93bbf9
Jerry Lott <jerry.l.lott@gmail.com>
2022-07-03 10:52:02 -0500

    Finished services list and added svg

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -13,9 +13,17 @@ export default function Services() {
             alt="Folded Clothes" 
             className="services-header__img"
           />
-          <div className="services-header__title">
-            <p class="center">Drop-off</p>
-            <p>Available MON - FRI</p>
+          <div className="services-header__info">
+            <div className="services-header__title">
+              <p class="head">Drop-off</p>
+              <p class="sub-head">Available MON - FRI</p>
+            </div>
+            <ul className="services-header__list">
+              <li>Just drop off your clothes and we will take care of the rest.</li>
+              <li>Supplies are included. Detergent, softener, etc.</li>
+              <li>The service includes washing, drying, and folding.</li>
+              <li>Satisfaction Guaranteed!</li>
+            </ul>
           </div>
         </div>
         <div className="services__btns">
