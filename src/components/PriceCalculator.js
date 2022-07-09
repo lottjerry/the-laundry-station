b5c380f9f4da23c8329baf929ab48d663380c411
Jerry Lott <jerry.l.lott@gmail.com>
2022-07-09 13:00:11 -0500

    Finished logic for calculator. Completed services section.

--- src/components/PriceCalculator.js
+++ src/components/PriceCalculator.js
@@ -6,9 +6,34 @@ export default function PriceCalculator(){
   const[weight, setWeight] = useState(0)
   const[size, setSize] = useState(0)
 
+
   const calcPrice = () => {
-    let totalPrice = (1.39 * weight).toFixed(2)
-    setPrice(totalPrice)
+    let weightPrice = 1.39 * weight
+    let totalPrice 
+    let sizePrice
+    
+    switch (size) {
+      case "small":
+        sizePrice = 24
+        break
+      case "medium":
+        sizePrice = 34
+        break
+      case "large":
+        sizePrice = 35
+        break 
+      default:
+        sizePrice = weightPrice
+        break
+    }
+
+    if (weightPrice > sizePrice) {
+      totalPrice = sizePrice
+    } else {
+      totalPrice = weightPrice
+    }
+    
+    setPrice(totalPrice.toFixed(2))
   }
 
   return (
@@ -16,25 +41,26 @@ export default function PriceCalculator(){
       <div className="calculator__title">Price Calculator</div>
       
       <div className="form">
+        <label className="basket-select__label">
+          Basket Size:
+          <select value={size} onChange={e => setSize(e.target.value)}className="basket-select__input">
+            <option value="none">None</option>
+            <option value="small">Small</option>
+            <option value="medium">Medium</option>
+            <option value="large">Large</option>
+          </select>
+        </label>
         <label className="basket-weight__label">Basket Weight: 
           <input 
             className="basket-weight__input"
             type="text" 
             placeholder="0"
-            maxlength="3"
+            maxlength="6"
             min="1"
-            max="999"
+            max="999.99"
             onChange={e => setWeight(e.target.value)}
           />
           lbs
-        </label>
-        <label className="basket-select__label">
-          Basket Size:
-          <select value={size} onChange={e => setSize(e.target.value)}className="basket-select__input">
-            <option value="24">Small</option>
-            <option value="34">Medium</option>
-            <option value="35">Large</option>
-          </select>
         </label>
         <div className="button" onClick={calcPrice}>
             <div className="button__text">Calculate</div>
