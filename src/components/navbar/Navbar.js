7b5beae50b201e6e801d0c82dcaafe6781b31df8
Jerry Lott <jerry.l.lott@gmail.com>
2022-08-02 23:33:26 -0500

    Finished adding responsive mobile nav bar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,8 +1,16 @@
 import ScrollspyNav from "react-scrollspy-nav";
 import "./navbar.scss";
+import { UilApps, UilTimes } from "@iconscout/react-unicons";
+import { useState } from "react";
 
 export default function Navbar() {
+	const [click, setClick] = useState(false);
+
+	const handleClick = () => setClick(!click);
+	const closeMobileMenu = () => setClick(false);
+
 	const scrollToTop = () => {
+		setClick(false);
 		window.scrollTo({
 			top: 0,
 			behavior: "smooth",
@@ -14,29 +22,36 @@ export default function Navbar() {
 			<div onClick={scrollToTop} className="nav__title">
 				The Laundry Station
 			</div>
+			<div className="menu__icon" onClick={handleClick}>
+				{click ? <UilTimes /> : <UilApps />}
+			</div>
 			<ScrollspyNav
 				scrollDuration="500"
 				scrollTargetIds={["home", "about", "services", "contact"]}
 				activeNavClass="active-item"
 			>
-				<ul className="nav__list">
-					<li>
-						<a href="#home" className="nav__item active-item">
+				<ul className={click ? "nav__menu active" : "nav__menu"}>
+					<li className="nav__item">
+						<a
+							href="#home"
+							className="nav__link active-item"
+							onClick={closeMobileMenu}
+						>
 							Home
 						</a>
 					</li>
-					<li>
-						<a href="#about" className="nav__item">
+					<li className="nav__item">
+						<a href="#about" className="nav__link" onClick={closeMobileMenu}>
 							About
 						</a>
 					</li>
-					<li>
-						<a href="#services" className="nav__item">
+					<li className="nav__item">
+						<a href="#services" className="nav__link" onClick={closeMobileMenu}>
 							Services
 						</a>
 					</li>
-					<li>
-						<a href="#contact" className="nav__item">
+					<li className="nav__item">
+						<a href="#contact" className="nav__link" onClick={closeMobileMenu}>
 							Contact Us
 						</a>
 					</li>
