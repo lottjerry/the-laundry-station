c879c5e1ea40a3efa39514760e5b09d33535640b
Jerry Lott <jerry.l.lott@gmail.com>
2022-08-13 23:59:47 -0500

    Reverted changes back to working state

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -19,7 +19,7 @@ export default function Navbar() {
 
 	return (
 		<nav className="nav" id="nav">
-			<div onClick={scrollToTop} className="nav__logo">
+			<div onClick={scrollToTop} className="nav__title">
 				The Laundry Station
 			</div>
 			<div className="menu__icon" onClick={handleClick}>
